cc942dbf0d913521d89bf165a2217d2b4e672812
molakesizhanfangguangmang <cccccclrrrrrr@163.com>
2026-08-25 03:48:49 +0800

    Remove v2ray configurations from OpenWRT build
    Remove v2ray-related configurations and update OpenWRT defconfig.

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,15 @@ git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCP
 # 北大源
 cp -r "$GITHUB_WORKSPACE/scripts/files-8916" "$GITHUB_WORKSPACE/openwrt/files"
 ls -R "$GITHUB_WORKSPACE/openwrt/files"
+cd openwrt
+# 先强制抹掉v2ray相关行
+sed -i '/CONFIG_PACKAGE_v2ray-core/d' .config
+sed -i '/CONFIG_PACKAGE_v2ray-geodata/d' .config
+echo "# CONFIG_PACKAGE_v2ray-core is not set" >> .config
+echo "# CONFIG_PACKAGE_v2ray-geodata is not set" >> .config
+
+# 关键，让openwrt kconfig系统重新解析配置
+make defconfig
+
+# 打印到action日志，确认是否关闭成功
+grep v2ray-core .config
